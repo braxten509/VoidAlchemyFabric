9ecc35542506b6e864e8b90407f901c141f95271
braxten509 <braxten509@hotmail.com>
2023-02-28 02:30:26 -0700

    Added files

--- src/main/java/net/braxtenchenay/voidalchemy/VoidAlchemy.java
+++ src/main/java/net/braxtenchenay/voidalchemy/VoidAlchemy.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//test
 public class VoidAlchemy implements ModInitializer {
 	public static final String MOD_ID = "voidalchemy";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
